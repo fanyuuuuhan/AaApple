a3dc70e8d5d581ecf102c0710531ba4e434c3f9c
fanyuuuhan <s111219045@stu.ntue.edu.tw>
2025-11-10 00:53:11 +0800

    主角攻擊與怪物碰撞

--- Assets/Scriptes/MonsterHit.cs
+++ Assets/Scriptes/MonsterHit.cs
@@ -6,16 +6,55 @@ public class MonsterHit : MonoBehaviour
     SpriteRenderer sr;
     float direction = 1;
     float speed = 0.5f;
+
+    //無敵時間設置
+    public float noHitTime = 0.5f;
+    bool noHit = false;
+
+    int hp = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sr=GetComponent<SpriteRenderer>();
         rb=GetComponent<Rigidbody2D>();
+        hp = 5;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "closeAttack")
+        {
+            hp -= 3;
+            print(hp);
+
+            noHit = true;
+            Invoke(nameof(ResetHit), noHitTime); // 自動在 noHitTime 秒後解除無敵
+        }
+        if (coll.gameObject.tag == "farAttack")
+        {
+            hp -= 2;
+            print(hp);
+
+            noHit = true;
+            Invoke(nameof(ResetHit), noHitTime); // 自動在 noHitTime 秒後解除無敵
+        }
+    }
+
+    void ResetHit()
+    {
+        noHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
         sr.flipX = true;
         transform.position += new Vector3(speed * direction * Time.deltaTime, 0, 0);
 
@@ -28,5 +67,7 @@ public class MonsterHit : MonoBehaviour
             direction = 1;
             sr.flipX=true;
         }
+
+        
     }
 }
